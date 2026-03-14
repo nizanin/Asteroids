1dc91ccfdda1eb07c8a805edab85a7f1fcf8bd8b
nizanin <nizanin8@gmail.com>
2026-03-14 19:20:20 +0100

    update game menu

--- main.py
+++ main.py
@@ -8,6 +8,38 @@ from logger import log_state
 import game_state
 
 
+def game_over_screen(screen, font, score):
+    while True:
+        screen.fill((0, 0, 0))
+
+        text1 = font.render("GAME OVER", True, (255, 0, 0))
+        text2 = font.render(f"Score: {score}", True, (255, 255, 255))
+        text3 = font.render("ENTER - restart", True, (255, 255, 255))
+        text4 = font.render("ESC - quit", True, (255, 255, 255))
+
+        screen.blit(text1, (350, 250))
+        screen.blit(text2, (350, 300))
+        screen.blit(text3, (350, 350))
+        screen.blit(text4, (350, 400))
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    exit()
+
+                if event.key == pygame.K_RETURN:
+                    return   # restart gry
+
+
+
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,48 +47,60 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    font = pygame.font.Font(None, 50)
 
-    clock = pygame.time.Clock()
-    dt = 0
+    while True:   # <-- pętla restartu gry
 
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    asteroids = pygame.sprite.Group()
-    bullets = pygame.sprite.Group()
+        game_state.SCORE = 0
+        clock = pygame.time.Clock()
+        dt = 0
+        log_timer = 0
 
-    Player.containers = (updatable, drawable)
-    Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
-    Shot.containers = (updatable, drawable, bullets)
-    
+        updatable = pygame.sprite.Group()
+        drawable = pygame.sprite.Group()
+        asteroids = pygame.sprite.Group()
+        bullets = pygame.sprite.Group()
 
-
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-
-    asteroid_field = AsteroidField()
-
-    while True:
-        log_state()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill(color="black")
+        Player.containers = (updatable, drawable)
+        Asteroid.containers = (asteroids, updatable, drawable)
+        AsteroidField.containers = (updatable)
+        Shot.containers = (updatable, drawable, bullets)
         
-        for item in drawable:
-            item.draw(screen)  
-        updatable.update(dt)
-        for asteroid in asteroids:
-            if asteroid.collision(player):
-                print("Game over!")
-                print(f"Your score: {game_state.SCORE}")
-                return
-            for bullet in bullets:
-                if asteroid.collision(bullet):
-                    asteroid.split()
-                    bullet.kill()
+
+
+        player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+        asteroid_field = AsteroidField()
+
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+            screen.fill(color="black")
             
-        pygame.display.flip()
-        dt = clock.tick(60)/1000
+            for item in drawable:
+                item.draw(screen)  
+            updatable.update(dt)
+
+            log_timer += dt
+            if log_timer > 1:
+                log_state()
+                log_timer = 0
+
+            for asteroid in asteroids:
+                if asteroid.collision(player):
+                    game_over_screen(screen, font, game_state.SCORE)
+                    running = False
+                    break
+                for bullet in bullets:
+                    if asteroid.collision(bullet):
+                        asteroid.split()
+                        bullet.kill()
+                
+            pygame.display.flip()
+            dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
